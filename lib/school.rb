aa7118606f13eb38daaf116bc367b15aac67b55e
fadimata tillman <fadi.tillman@gmail.com>
2020-02-10 19:40:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
-# code here!
+class School
+def initialize(roster)
+  @roster = {}
+end
+def roster
+  @roster
+end
+def add_student (name,grade)
+ @roster[grade] ? @roster[grade]<< name : @roster[grade] =[name]
+
+  # student_hash = {grade => [name]}
+  # @roster = student_hash
+  # @roster.map do |st_num,st_name|
+# if @roster[grade.two_sym]
+end
+def grade (grade)
+  @roster[add_student][grade].include?    @roster[add_student][grade]<< @roster[new.add_student][grade]
+end
+end
